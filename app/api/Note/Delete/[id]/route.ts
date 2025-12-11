582745c1f5d0f0d5a215d69477d064e348b444fe
Rusira54321 <rusiradinujaya57@gmail.com>
2025-12-11 10:20:52 +0530

    create delete functionality

---
+++ app/api/Note/Delete/[id]/route.ts
@@ -0,0 +1,26 @@
+import { NextRequest, NextResponse } from "next/server";
+import {prisma} from "../../../../../lib/prisma"
+export async function DELETE(request:NextRequest,context:{params:Promise<{id:string}>})
+{
+    try{
+        const {id} = await context.params;
+        const noteId = parseInt(id)
+        if(isNaN(noteId))
+        {
+            return NextResponse.json({error:`Invalid product ID,${id}`},{status:400});
+        }
+        await prisma.post.delete({
+            where:{
+                id:noteId
+            },
+        })
+        return NextResponse.json({message:"note deleted successfully"},{status:200});
+    }catch(err)
+    {
+        if(err instanceof Error)
+        {
+             return NextResponse.json({error:err.message},{status:500});
+        }
+        return NextResponse.json({error:"Database error"},{status:500});
+    }
+}
--- app/components/DeleteButton.tsx
+++ app/components/DeleteButton.tsx
@@ -1,11 +1,26 @@
 'use client'
+import axios from 'axios';
 import React from 'react'
 type DeleteButtonProps = {
     id:number;
 }
 const DeleteButton = ({id}:DeleteButtonProps) => {
   const handleDelete = async() =>{
-        
+      try{
+        console.log(id)
+          await axios.delete(`/api/Note/Delete/${id}`)
+          alert("Delete Successfully");
+          window.location.reload();
+      }catch(err:unknown)
+      {
+         if (axios.isAxiosError(err)) {
+          alert(err?.response?.data?.error || 'Axios error occurred');
+        } else if (err instanceof Error) {
+          alert(err?.message);
+        } else {
+          alert('Something went wrong');
+        }
+      }
   }
   return (
    <button onClick={handleDelete} className='bg-red-500 hover:bg-red-600 rounded-2xl px-2 cursor-pointer'>Delete</button>
